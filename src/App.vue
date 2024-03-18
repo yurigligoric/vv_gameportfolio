0cfb251e33d9d6e42b372664de95243378ca652f
yurigligoric <yurigligoric@gmail.com>
2024-03-18 11:19:43 +0800

    first commit vite vue tailwind setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+
+      <p className="font-bold bg-gray-600">Hello World</p>
+      
+    
+
+  </div>
+  
+</template>
+
+<style>
+
+</style>
